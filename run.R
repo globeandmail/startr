c5e310ea330a4b14766c289fb4687ff48d5d54dd
Tom Cardoso <tcardoso@globeandmail.com>
2019-07-08 13:45:02 -0400

    Update run.R

--- run.R
+++ run.R
@@ -2,8 +2,8 @@ if (!require('devtools')) install.packages('devtools'); library('devtools')
 if (!require('here')) install.packages('here'); library('here')
 
 source(here::here('config.R'))
-if (should_process_data){ source(here::here(dir_src, 'process.R')) }
+if (should_process_data) { source(here::here(dir_src, 'process.R')) }
 source(here::here(dir_src, 'analyze.R'))
 source(here::here(dir_src, 'visualize.R'))
 
-if (should_render_notebook){ render_notebook(r_notebook) }
+if (should_render_notebook) { render_notebook(r_notebook) }
