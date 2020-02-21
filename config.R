d7cd8bf9ba85c91731d3d7dcba40438e5925c823
Tom Cardoso <tcardoso@globeandmail.com>
2020-02-21 12:57:20 -0500

    a new idea for how to set up the config file

--- config.R
+++ config.R
@@ -4,90 +4,72 @@
 #
 ############################################################
 
-options(scipen = 999)
-Sys.setenv(TZ = 'America/Toronto')
+options(
+  startr.config_author = 'Firstname Lastname <firstlast@globeandmail.com>',
+  startr.config_title = 'startr',
+  startr.config_timezone = 'America/Toronto',
+  startr.config_dir_data = 'data',
+  startr.config_dir_src = 'R',
+  startr.config_dir_data_raw = 'data/raw',
+  startr.config_dir_data_cache = 'data/cache',
+  startr.config_dir_data_processed = 'data/processed',
+  startr.config_dir_data_out = 'data/out',
+  startr.config_dir_reports = 'reports',
+  startr.config_dir_plots = 'plots',
+  startr.config_should_render_notebook = FALSE, # Should a notebook be rendered in run.R?
+  startr.config_should_process_data = TRUE, # Should the processing step be run in run.R?
+  startr.config_timestamp_output_files = FALSE, # Should files written with write_excel have a timestamp in the filename?
+  startr.config_clean_processing_variables = TRUE, # Should the variables created during process.R be cleaned up after processing?
+  startr.config_r_notebook = dir_reports('notebook.Rmd'),
+  startr.config_knitr = list(
+    eval = TRUE,
+    echo = FALSE,
+    message = FALSE,
+    cache = FALSE,
+    warning = FALSE,
+    error = FALSE,
+    comment = '#',
+    tidy = FALSE,
+    collapse = TRUE,
+    results = 'asis',
+    fig.width = 12,
+    dpi = 150,
+    root.dir = dir_here()
+  ),
+  startr.config_packages = c(
+    # essentials
+    'here', 'devtools', 'tidyverse', 'startr',
+    # manipulation
+    'lubridate', 'janitor', 'zoo', 'glue', 'clipr', 'beepr',
+    # modelling
+    'tidymodels',
+    # Read/write files
+    'readxl', 'openxlsx', 'feather',
+    # visualization
+    'scales', 'ggthemes', 'gganimate',
+    # scraping
+    'rvest',
+    # GIS
+    'sf',
+    # RMarkdown
+    'knitr', 'ezknitr', 'kableExtra', 'DT',
+    # other stuff
+    'cansim', 'cancensus'
+  ),
+  # CANCENSUS_API should be set in your home directory's .Renviron file
+  cancensus.api_key = Sys.getenv(c('CANCENSUS_API')),
+  cancensus.cache = dir_data_cache(),
+  cansim.cache_path = dir_data_cache(),
+  scipen = 999
+)
 
-# Project-specific
-config_author <- 'Firstname Lastname <firstlast@globeandmail.com>'
-config_title <- 'startr'
-
-# Directories to read from and write to
-dir_data <- 'data'
-dir_src <- 'R'
-dir_data_raw <- 'data/raw'
-dir_data_cache <- 'data/cache'
-dir_data_processed <- 'data/processed'
-dir_data_out <- 'data/out'
-dir_reports <- 'reports'
-dir_plots <- 'plots'
+initialize_startr()
+# does several things:
+# 1. installs default core packages so that we can remove them from the package object
+# 2. consumes the startr config stuff to set up the environment
+# 3. exposes all the functions listed in utils, plus a few more TK, namely:
+#      - initialize_startr()
+#      - dir_data_* functions
 
 # Files: You'll want to edit this to add your source data file names
 sample.raw.file <- here::here(dir_data_raw, 'sample.csv')
-
-# Primary and supplemental notebooks.
-# Set should_render_notebook to TRUE if using notebooks
-r_notebook <- here::here(dir_reports, 'notebook.Rmd')
-
-# startr-specific configuration, consumed by helper functions
-# Should a notebook be rendered in run.R?
-should_render_notebook <- FALSE
-
-# Should the processing step be run in run.R?
-should_process_data <- TRUE
-
-# Should files written with write_excel have a timestamp in the filename?
-timestamp_output_files <- FALSE
-
-# Should the variables created during process.R be cleaned up after processing?
-clean_processing_variables <- TRUE
-
-packages <- c(
-  # essentials
-  'here', 'devtools', 'tidyverse',
-  # manipulation
-  'lubridate', 'janitor', 'zoo', 'glue', 'clipr', 
-  # modelling
-  'tidymodels',
-  # Read/write files
-  'readxl', 'openxlsx', 'feather',
-  # visualization
-  'scales', 'ggthemes', 'gganimate',
-  # scraping
-  'rvest',
-  # GIS
-  'sf',
-  # RMarkdown
-  'knitr', 'ezknitr', 'kableExtra', 'DT',
-  # other stuff
-  # 'cansim', 'cancensus',
-  'beepr'
-)
-
-source(here::here(dir_src, 'utils.R'))
-source(here::here(dir_src, 'functions.R'))
-
-load_requirements(packages)
-
-options(
-  # CANCENSUS_API should be set in your home directory's .Renviron file,
-  # and will get pulled down from there
-  cancensus.api_key = Sys.getenv(c('CANCENSUS_API')),
-  cancensus.cache = here::here(dir_data_cache),
-  cansim.cache_path = here::here(dir_data_cache)
-)
-
-knitr::opts_chunk$set(
-  eval = TRUE,
-  echo = FALSE,
-  message = FALSE,
-  cache = FALSE,
-  warning = FALSE,
-  error = FALSE,
-  comment = '#',
-  tidy = FALSE,
-  collapse = TRUE,
-  results = 'asis',
-  fig.width = 12,
-  dpi = 150,
-  root.dir = here::here()
-)
