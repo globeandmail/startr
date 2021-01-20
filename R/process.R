056fca43ecc967ceb330561c42b66e32659bc113
Tom Cardoso <tcardoso@globeandmail.com>
2021-01-20 13:09:25 -0500

    adding stuff

--- R/process.R
+++ R/process.R
@@ -2,8 +2,8 @@
 # Read raw data, clean it and save it out to `dir_data_processed()` here
 # before moving to analysis. If run from `run.R`, all variables generated
 # in this file will be wiped after completion to keep the environment
-# clean. If your processing is complex, you can break it out into several
-# files like this: `source(dir_src('process_files', 'process_step_1.R'))`
+# clean. If your process step is complex, you can break it into several
+# files like so: `source(dir_src('process_files', 'process_step_1.R'))`
 # =======================================================================
 
 # sample.raw <- read_csv(sample.raw.file) %>%
