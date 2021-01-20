31d41dfc23facf6dde4c0104eb10a066c23d74e5
Tom Cardoso <tcardoso@globeandmail.com>
2021-01-19 19:12:50 -0500

    simplifying note

--- R/process.R
+++ R/process.R
@@ -1,11 +1,9 @@
 # =======================================================================
-# This file is used to read in raw data, clean it, and save a file to
-# `dir_data_processed()` before proceeding to analysis. If this file is
-# run from run.R, all variables created by this step will be erased after
-# the step is complete to keep a clean working environment. Tip: If your
-# analysis is complicated enough that you need to break the processing
-# out into multiple files, simply source them from this file by calling
-# something like `source(dir_src('process_files', 'process_step_1.R'))`
+# Read raw data, clean it and save it out to `dir_data_processed()` here
+# before moving to analysis. If run from `run.R`, all variables generated
+# in this file will be wiped after completion to keep the environment
+# clean. If your processing is complex, you can break it out into several
+# files like this: `source(dir_src('process_files', 'process_step_1.R'))`
 # =======================================================================
 
 # sample.raw <- read_csv(sample.raw.file) %>%
