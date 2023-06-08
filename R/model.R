86b9274c7d0b64921784a683f5bf9dd615a5ea81
Michael Pereira <monkeycycle@gmail.com>
2023-06-08 14:20:29 -0500

    Adds model.R file for regressions etc.

---
+++ R/model.R
@@ -0,0 +1,17 @@
+# =======================================================================
+# Run a linear regression on the data and-
+# check the model's performance and coefficients.
+#
+# Basic regression syntax is-
+# lm([target] ~ [predictor / features], data = [data source])
+#
+# =======================================================================
+
+# This is a single variable regression based on available data from sample
+# sales_cma <- lm(sale_avg_3mo ~ cma, data = sample)
+# summary(sales_cma)
+
+# Plot the residuals
+# plot(sales_cma$residuals, pch = 16, col = "red")
+
+~
